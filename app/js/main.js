fcb4d955d5ee3bfafbb06847ea30532d0f07bf23
dmitriy-works <dmitriy-works@ukr.net>
2023-05-09 13:01:54 +0300

    add header

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,15 @@
-function browsersync() {
-  browserSync.init({
-    server: {
-      baseDir: "app/"
-    }
-  });
-}
+$(function () {
+  
+
+const menuBurger = document.querySelector('.burger-menu');
+const menuList = document.querySelector('.header__menu-list');
+
+console.log(menuBurger)
+
+menuBurger.addEventListener('click', function () {
+  menuList.classList.toggle('header__list--active');
+});
+
+
+
+});
